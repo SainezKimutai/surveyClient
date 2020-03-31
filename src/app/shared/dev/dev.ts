6417a19e769b76913bb1d815d9909aafb985d36a
GeofreyMuindeMunguti <geofrey.muinde@stratgmore.edu>
2020-03-31 13:07:07 +0300

    status before dashboards pull

--- src/app/shared/dev/dev.ts
+++ src/app/shared/dev/dev.ts
@@ -5,5 +5,5 @@ export const header = new HttpHeaders().set(
     'Authorization', `Bearer ${window.localStorage.getItem('loggedUserToken')}`
   );
 export const dev = {
-    connect: 'http://167.172.26.3:4111/',
+    connect: 'https://surveyserver.imprintafrica.co.ke/',
 };
