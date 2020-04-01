51967b1ebf2730b4a1ae68f83afe95d0d49267c1
Sainezkimutai <sainezkimutai@gmail.com>
2020-04-01 21:52:02 +0300

    updates

--- src/app/components/home/reports/reports.component.ts
+++ src/app/components/home/reports/reports.component.ts
@@ -7,6 +7,11 @@ import { QuestionService } from 'src/app/shared/services/questions.service';
 import { ThreatService } from 'src/app/shared/services/threats.service';
 import { faListAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
 
+import * as jspdf from 'jspdf';
+import 'jspdf-autotable';
+
+declare let html2canvas: any;
+
 
 @Component({
   selector: 'app-reports',
@@ -30,8 +35,9 @@ export class ReportsComponent implements OnInit {
       public faDownload = faDownload;
     
       //
-      public AllSurveys = [];//keep and rename..
+      public AllSurveys: any;//keep and rename..
       public AllResponses =[];
+      public AllThreats = [];
       public AllQuestions = [];
       public TemplateNameOnView = [];
       public TemplateQuestions = []; // keep and rename.
@@ -40,8 +46,16 @@ export class ReportsComponent implements OnInit {
 
       ngOnInit() {
         localStorage.setItem('ActiveNav', 'reports');
+        this.threatService.getAllThreats().subscribe(
+          data => this.AllThreats = data,
+          error => console.log('Cannot get all error')
+        )
         this.updatePage();
       }
+
+
+
+
 
       updatePage() {
         return new Promise((resolve, reject) => {
@@ -49,7 +63,7 @@ export class ReportsComponent implements OnInit {
           this.responseService.getUsersResponses(localStorage.getItem('loggedUserID')).subscribe(
             data => 
             { 
-             
+             this.AllSurveys = [];
               data.forEach(responseObj => {
                     this.surveyService.getOneSurvey(responseObj.surveyId).subscribe(
                      survey =>{
@@ -67,46 +81,41 @@ export class ReportsComponent implements OnInit {
                                question['question'] = questions.question
                                   if(answr.answer.length == 1){
                                   
-                                  answr.answer.forEach(answr => {
-                                  
-
-                                    question['answer'] = answr.answer ? answr.answer: answr;
-                                    question['recom'] = answr.recom ? answr.recom: '';
-                                    question['level'] = answr.level ? answr.level : '';
-                                    
-
-                                    if(typeof(question['answer']) === 'object'){
+                                      answr.answer.forEach(answr => {
                                       
-                                      question['answer'] = answr.answer.answer;
-                                      question['recom'] = answr.answer.recom;
-                                      question['level'] = answr.answer.level;
-                              
-                                    }
 
-                                    this.AllQuestions.push(question);
-                                     
-                                  });
+                                        question['answer'] = answr.answer ? answr.answer: answr;
+                                        question['recom'] = answr.recom ? answr.recom: '';
+                                        question['level'] = answr.level ? answr.level : '';
+                                        
+                                    
+                                      
+                                        this.AllQuestions.push(question);
+                                  
+                                        
+                                      });
                                 
-                            }
-                            if(answr.answer.length>1){
+                                  }
+                                  if(answr.answer.length>1){
 
-                                question['answer'] = '';
-                                question['recom'] = '';
-                                question['level'] = '';
+                                          question['answer'] = '';
+                                          question['recom'] = '';
+                                          question['level'] = '';
 
-                              for(var i =0; i < answr.answer.length ; i++){
-                                
+                                        for(var i =0; i < answr.answer.length ; i++){
+                                          
 
-                                question['answer'] = answr.answer[i].answer ? question['answer'] +" "+answr.answer[i].answer: '';
-                                question['recom'] = answr.answer[i].recom ? question['recom'] + " " + answr.answer[i].recom: '';
-                                question['level'] = answr.answer[i].level ? question['level'] + " " + answr.answer[i].level : '';
-                                
-                                if(i === answr.answer.length-1){
-                                this.AllQuestions.push(question);
-                                }
-                              }
-              
-                            }
+                                          question['answer'] = answr.answer[i].answer ? question['answer'] +" "+answr.answer[i].answer: '';
+                                          question['recom'] = answr.answer[i].recom ? question['recom'] + " " + answr.answer[i].recom: '';
+                                          question['level'] = answr.answer[i].level ? question['level'] + " " + answr.answer[i].level : '';
+                                        
+                                          
+                                          if(i === answr.answer.length-1){
+                                          this.AllQuestions.push(question);
+                                          }
+                                        }
+                        
+                                  }
                     }
 
                     )
@@ -133,9 +142,86 @@ export class ReportsComponent implements OnInit {
         let unSortedQuestions = this.AllQuestions.filter(( quiz) => quiz.surveyId === id ).map(e => e);
         // console.log(unSortedQuestions);
         this.TemplateQuestions = unSortedQuestions.sort((a, b) =>  a.position - b.position);
+    
         this.TemeplateViewSectionStatus = false;
         this.QuestionsViewStatus = true;
     
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+  async downloadPdf() {
+    this.ImprintLoader = true;
+    this.notifyService.showInfo('..downloading', 'Pdf template')
+    setTimeout(() => {
+      
+    let pageQuestions = 3;
+    let pageNumber = 1;
+    let reportConstantStartPosition = 30;
+
+    const pdf = new jspdf('p', 'mm', 'a4');
+
+    pdf.text(75, 20, `Risk Analysis Report`);
+    pdf.setFontSize(12);
+    let img = new Image();
+    img.src = 'assets/images/logo1.jpg'; 
+    pdf.addImage(img, 'PNG', 14, 10, 20, 16);
+
+    this.TemplateQuestions.forEach((quiz, key, arr) => {
+          
+        const reportTemplate = html2canvas(document.querySelector(`#quiz${key}`), {scale: 3});   
+
+        reportTemplate.then(reportCanvas => {
+
+  
+          let reportFinalStartPosition = Number(reportConstantStartPosition)
+
+
+          if ( quiz.recom) {
+            pdf.addImage(reportCanvas, 10, reportFinalStartPosition, 190, 60);
+            reportConstantStartPosition = reportConstantStartPosition + 65
+          }
+          if (!quiz.recom) {
+            pdf.addImage(reportCanvas, 10, reportFinalStartPosition, 190, 30);
+            reportConstantStartPosition = reportConstantStartPosition + 35
+          }
+
+
+          if ( key > pageQuestions) {
+                pageNumber = pageNumber + 1;
+                pageQuestions = pageQuestions + 4;
+                reportConstantStartPosition = 30
+                pdf.addPage();    
+     
+          }
+
+
+          if (Object.is(arr.length - 1, key)) {
+
+            pdf.save('RiskAnalysisReport.pdf');
+            // pdf.output('dataurlnewwindow');
+            this.ImprintLoader = false;
+          }
+
+        }); 
+    } )
+  }, 50);
+
+
+  }
+
+
+
+
+
+
+} // Main
     
