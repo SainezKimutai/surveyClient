d24873583e7eddf4aa890ab3aafc4da3a2bcd46c
Sainezkimutai <sainezkimutai@gmail.com>
2020-07-23 13:53:15 +0300

    update

--- src/app/shared/dev/dev.ts
+++ src/app/shared/dev/dev.ts
@@ -27,7 +27,7 @@ export const preauthheader = new HttpHeaders().set('secrete', 'IMPRINT@@2020');
 // };
 
 // export const dev = {
-//     connect: 'http://167.172.26.3/:4411/',
+//     connect: 'https://surveyserver.imprintafrica.co.ke/',
 // };
 
 export const dev = {
